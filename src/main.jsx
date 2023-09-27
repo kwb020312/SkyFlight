5f0e0a6e3ded14c76e0e6e1dea2536b9a2cbf970
김우빈 <kwb020312@naver.com>
2023-09-27 18:52:10 +0900

    Feat: Add Model & Textures

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Canvas shadows>
+    <Suspense fallback={null}>
+      <App />
+    </Suspense>
+  </Canvas>
+);
