087a110e3032dc85567c14cb92a60319a49a9956
김우빈 <kwb020312@naver.com>
2023-09-29 00:30:57 +0900

    Feat: Add Camera Matrix

--- src/Airplane.jsx
+++ src/Airplane.jsx
@@ -1,5 +1,9 @@
 import { useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
+import { Matrix4, Vector3 } from "three";
+
+export const planePosition = new Vector3(0, 3, 7);
 
 // 모델을 만드신 berilbasak님께 감사드립니다.
 const Airplane = (props) => {
@@ -7,9 +11,37 @@ const Airplane = (props) => {
   const groupRef = useRef();
   const helixMeshRef = useRef();
 
+  // 비행기의 시작점 위치를 4차원 벡터 행렬을 사용하여 위치시킴
+  useFrame(({ camera }) => {
+    const matrix = new Matrix4().makeTranslation(
+      planePosition.x,
+      planePosition.y,
+      planePosition.z
+    );
+
+    groupRef.current.matrixAutoUpdate = false;
+    groupRef.current.matrix.copy(matrix);
+    groupRef.current.matrixWorldNeedsUpdate = true;
+
+    const cameraMatrix = new Matrix4()
+      .multiply(
+        new Matrix4().makeTranslation(
+          planePosition.x,
+          planePosition.y,
+          planePosition.z
+        )
+      )
+      .multiply(new Matrix4().makeRotationX(-0.2))
+      .multiply(new Matrix4().makeTranslation(0, 0.015, 0.3));
+
+    camera.matrixAutoUpdate = false;
+    camera.matrix.copy(cameraMatrix);
+    camera.matrixWorldNeedsUpdate = true;
+  });
+
   return (
     <>
-      <group ref={groupRef}>
+      <group ref={groupRef} position={[0, 0, 0]}>
         <group {...props} dispose={null} scale={0.01} rotation-y={Math.PI}>
           <mesh
             geometry={nodes.supports.geometry}
--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
       {/* 카메라를 구체보다 가까이 위치하게 하고 컨트롤러를 구 내부에 위치함으로써 */}
       {/* 구체 표면 모델이 마치 풍경이 된 것 같은 효과를 줌 */}
       <PerspectiveCamera makeDefault position={[0, 10, 10]} />
-      <OrbitControls target={[0, 0, 0]} />
+      {/* <OrbitControls target={[0, 0, 0]} /> */}
 
       {/* 지면 데이터 총괄 */}
       <Landscape />
